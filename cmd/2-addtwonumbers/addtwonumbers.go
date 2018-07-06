d14165b17a139fcdcdd7855dc7a44c6708bb9a8b
Mark Stuart <stuarmark@gmail.com>
2018-07-05 20:59:36 -0400

    Adding home edits

--- cmd/2-addtwonumbers/addtwonumbers.go
+++ cmd/2-addtwonumbers/addtwonumbers.go
@@ -11,43 +11,84 @@ type ListNode struct {
 }
 
 func main() {
+
 	numarr1 := [3]int{2, 4, 3}
 	numarr2 := [3]int{5, 6, 4}
+
+	// numarr1 := [2]int{9, 8}
+	// numarr2 := [1]int{1}
 
 	l1c := ListNode{Val: numarr1[2], Next: nil}
 	l1b := ListNode{Val: numarr1[1], Next: &l1c}
 	l1 := ListNode{Val: numarr1[0], Next: &l1b}
+	// l1b := ListNode{Val: numarr1[1], Next: nil}
 
 	l2c := ListNode{Val: numarr2[2], Next: nil}
 	l2b := ListNode{Val: numarr2[1], Next: &l2c}
 	l2 := ListNode{Val: numarr2[0], Next: &l2b}
+	// l2 := ListNode{Val: numarr2[0], Next: nil}
+
+	// l1 := ListNode{Val: 5, Next: nil}
+
+	// l2 := ListNode{Val: 5, Next: nil}
 
 	retNode := addTwoNumbers(&l1, &l2)
-
-	fmt.Println(retNode)
+	testLinkedList(retNode)
+	//fmt.Println(retNode)
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	const dec = 0
-	a, b := 0, 0
+	a, b, carryover := 0, 0, 0
+	listNodeRet := &ListNode{Val: ((l1.Val + l2.Val) % 10)}
+	headNode := listNodeRet
+
+	if (l1.Val + l2.Val) > 9 {
+		carryover = 1
+	}
 
 	for {
-		fmt.Println(fmt.Sprintf("l1 is: %d", l1.Val))
-		fmt.Println(fmt.Sprintf("l2 is: %d", l2.Val))
 		if l1.Next != nil {
 			l1 = l1.Next
 		} else {
 			a = 1
+			l1.Val = 0
 		}
 		if l2.Next != nil {
 			l2 = l2.Next
 		} else {
 			b = 1
+			l2.Val = 0
 		}
-
 		if a == 1 && b == 1 {
 			break
 		}
+
+		ltemp := l1.Val + l2.Val + carryover
+		node := ListNode{Val: ltemp % 10, Next: nil}
+		listNodeRet.Next = &node
+
+		if ltemp > 9 {
+			carryover = 1
+		} else {
+			carryover = 0
+		}
+		listNodeRet = listNodeRet.Next
 	}
-	return nil
+	if carryover == 1 {
+		node := ListNode{Val: carryover, Next: nil}
+		listNodeRet.Next = &node
+	}
+
+	return headNode
+}
+
+func testLinkedList(list *ListNode) {
+	for {
+		fmt.Println(list)
+		if list.Next != nil {
+			list = list.Next
+		} else {
+			break
+		}
+	}
 }
