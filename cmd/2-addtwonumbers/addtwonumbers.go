ab2a204aa64a6383d6326b2a03af0e396afc6bcb
Mark Stuart <stuarmark@gmail.com>
2018-07-05 21:13:31 -0400

    Adding home edits

--- cmd/2-addtwonumbers/addtwonumbers.go
+++ cmd/2-addtwonumbers/addtwonumbers.go
@@ -12,29 +12,31 @@ type ListNode struct {
 
 func main() {
 
+	// testcase linked lists w/ equivalent lengths
 	numarr1 := [3]int{2, 4, 3}
 	numarr2 := [3]int{5, 6, 4}
-
-	// numarr1 := [2]int{9, 8}
-	// numarr2 := [1]int{1}
 
 	l1c := ListNode{Val: numarr1[2], Next: nil}
 	l1b := ListNode{Val: numarr1[1], Next: &l1c}
 	l1 := ListNode{Val: numarr1[0], Next: &l1b}
-	// l1b := ListNode{Val: numarr1[1], Next: nil}
 
 	l2c := ListNode{Val: numarr2[2], Next: nil}
 	l2b := ListNode{Val: numarr2[1], Next: &l2c}
 	l2 := ListNode{Val: numarr2[0], Next: &l2b}
+
+	// testcase linked lists w/ non-equivalent lengths
+	// numarr1 := [2]int{9, 8}
+	// numarr2 := [1]int{1}
+	// l1b := ListNode{Val: numarr1[1], Next: nil}
+	// l1 := ListNode{Val: numarr1[0], Next: &l1b}
 	// l2 := ListNode{Val: numarr2[0], Next: nil}
 
+	// testcase singel digit w/ carry over
 	// l1 := ListNode{Val: 5, Next: nil}
-
 	// l2 := ListNode{Val: 5, Next: nil}
 
 	retNode := addTwoNumbers(&l1, &l2)
 	testLinkedList(retNode)
-	//fmt.Println(retNode)
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
