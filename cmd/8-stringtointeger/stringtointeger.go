c776bf6f3997acfedafbfafbd298f2d4afed307e
Mark Stuart <stuarmark@gmail.com>
2018-08-17 21:21:32 -0400

    Passed all leetcode test cases

--- cmd/8-stringtointeger/stringtointeger.go
+++ cmd/8-stringtointeger/stringtointeger.go
@@ -7,35 +7,51 @@ import (
 
 func main() {
 
-	fmt.Println(myAtoi("-3219"))
+	fmt.Println(myAtoi("-   234"))
 }
 
 func myAtoi(str string) int {
-	sign, num := 1, 0
+	sign, num, signcount, numcount := 1, 0, 0, 0
+
 	if str == "" {
 		return 0
 	}
 
 	for i := 0; i < len(str); i++ {
 		if str[i] == ' ' {
+			if numcount > 0 || signcount > 0 {
+				break
+			}
+			continue
+		} else if str[i] == '+' {
+			if signcount > 0 {
+				break
+			}
+			signcount++
 			continue
 		} else if str[i] == '-' {
-			sign = -1
+			if signcount == 0 && numcount == 0 {
+				sign = -1
+				signcount++
+			} else if numcount > 0 || signcount > 0 {
+				break
+			}
+
 			continue
 		}
 		if str[i] < 48 || str[i] > 57 {
 			break
 		}
+
 		num = num*10 + int(str[i]-'0')
+		numcount++
 
+		if num > math.MaxInt32 && sign == -1 {
+			return math.MinInt32
+		} else if num > math.MaxInt32 {
+			return math.MaxInt32
+		}
 	}
-	num = sign * num
 
-	if num > math.MaxInt32 {
-		return math.MaxInt32
-	} else if num < math.MinInt32 {
-		return math.MinInt32
-	}
-
-	return num
+	return sign * num
 }
