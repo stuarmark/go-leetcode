d2ac87197a07da1cadcdfb5770da3c5af0a2be5f
Mark Stuart <stuarmark@gmail.com>
2018-12-01 22:39:28 -0500

    Adding home edits

--- cmd/9-palindromenumber/palindromenumber.go
+++ cmd/9-palindromenumber/palindromenumber.go
@@ -5,14 +5,26 @@ import (
 )
 
 func main() {
-	x := 1001
+	x := 1000000001
 	fmt.Println(isPalindrome(x))
 }
 
 func isPalindrome(x int) bool {
-	if x < 0 {
+	if x < 0 || x%10 == 0 {
 		return false
+	} else if x <= 9 {
+		return true
 	}
 
+	pnd := 0
+	for x > pnd {
+		x = x / 10
+		rem := x % 10
+		pnd = pnd*10 + rem
+
+		if x == pnd || x/10 == pnd {
+			return true
+		}
+	}
 	return false
 }
