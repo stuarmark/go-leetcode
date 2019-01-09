fa72e2766d08aa62b798905d4814b416942f80ef
Mark Stuart <stuarmark@gmail.com>
2019-01-08 21:13:35 -0500

    Adding home edits

---
+++ cmd/1-twosum/twosum.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(twoSum([]int{6, 1, 4, 3}, 9))
+}
+
+func twoSum(nums []int, target int) []int {
+	numsmap := make(map[int]int)
+	for i, v := range nums {
+		if j, k := numsmap[target-v]; k {
+			return []int{j, i}
+		} else {
+			numsmap[v] = i
+		}
+	}
+	return nil
+}
