20a65b06d841dba3b7fb6d222678dfc2f885b7ea
Mark Stuart <stuarmark@gmail.com>
2019-10-06 19:15:29 -0400

    Adding home edits

--- cmd/13-romantoint/romantoint.go
+++ cmd/13-romantoint/romantoint.go
@@ -3,9 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(romanToInt("test"))
+	fmt.Println(romanToInt("XXXIV"))
+}
+
+var value = map[rune]int{
+	'M': 1000,
+	'D': 500,
+	'C': 100,
+	'L': 50,
+	'X': 10,
+	'V': 5,
+	'I': 1,
 }
 
 func romanToInt(s string) int {
-	return 0
+	var sum int
+	var last rune
+	for i, r := range s {
+		v, l := value[r], value[last]
+		sum += v
+		last = r
+		if i > 0 && v > l {
+			sum -= l * 2
+		}
+	}
+	return sum
 }
